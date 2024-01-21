3dbaa8c78a907544307e07562a1cce4abf580151
Sachin-251 <sachin.251@outlook.com>
2024-01-22 03:36:47 +0530

    third commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/Google"
+import GoogleProvider from "next-auth/providers/google"
 import prisma from "./connect";
 import { getServerSession } from "next-auth";
 
