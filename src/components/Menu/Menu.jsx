95806784d35d8b5af0af621925ce85cb7c738441
Sachin-251 <sachin.251@outlook.com>
2024-01-22 03:52:09 +0530

    update 2

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,7 +7,7 @@ import MenuPopular from '../menuPopular/MenuPopular';
 function Menu() {
   return (
     <div className={styles.container}>
-      <h2 className={styles.subtitle}>What's Hot</h2>
+      <h2 className={styles.subtitle}>What&apos;s Hot</h2>
       <h1 className={styles.title}>Most Popular</h1>
       <MenuPopular />
 
@@ -16,7 +16,7 @@ function Menu() {
       <MenuCategories />
 
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
-      <h1 className={styles.title}>Editor's Pick</h1>
+      <h1 className={styles.title}>Editor&apos;s Pick</h1>
       <MenuPosts />
     </div>
   )
